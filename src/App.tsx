cda9c34994042d088190cee87c19e77613b936d8
jhlee <wisdom_13_@naver.com>
2024-01-28 20:37:46 +0900

    dnd 드래그앤드랍 예시

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import IntersectionObserverUI from './project/IntersectionObserverUI';
 import ModalUI from './project/ModalUI';
 import PaginationUI from './project/PaginationUI';
 import SkeletonUI from './project/SkeletonUI';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import Container from './components/Dnd/Container';
+
 
 export default function App() {
   return (
-    // <SkeletonUI />
-    // <Carousel />
-    // <PaginationUI />
-    // <ModalUI />
-    // <InfiniteScrollUI />
-    <IntersectionObserverUI />
+    <>
+      {/* <SkeletonUI />
+      <Carousel />
+      <PaginationUI />
+      <ModalUI />
+      <InfiniteScrollUI />
+      <IntersectionObserverUI /> */}
+      <DndProvider backend={HTML5Backend}>
+        <Container />
+      </DndProvider>
+    </>
   );
 }
 
---
+++ src/project/DndUI.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DndUI() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
